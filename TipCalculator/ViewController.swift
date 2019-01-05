01b3166160583ab135bca67c71dc175526503b57
Maksat Zhazbayev <longhorn18@maksats-macbook-pro.local>
2019-01-05 16:23:30 -0600

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Maksat Zhazbayev on 1/5/19.
+//  Copyright © 2019 Maksat Zhazbayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
